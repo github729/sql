45eb560c31bc7968cef93b75412b6362e4c4830e
github729 <42238918+github729@users.noreply.github.com>
2019-05-05 10:14:48 +0530

    Update jobs.sql

--- jobs.sql
+++ jobs.sql
@@ -350,7 +350,7 @@ CREATE TABLE `jobs` (
   `jobDeadLine` varchar(255) DEFAULT NULL,
   `industry` varchar(255) NOT NULL,
   `companyName` varchar(255) NOT NULL,
-  `companySize` int(11) DEFAULT NULL,
+  `companySize` int(11) DEFAULT 0,
   `companySite` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `mobileNumber` varchar(255) DEFAULT NULL,
